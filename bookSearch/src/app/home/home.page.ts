bd2a12cf3e73134439e5ed7ccb1fcf2246f90b2c
ruben <rbnm.jimenez@gmail.com>
2019-03-17 22:19:41 -0300

    adding request to server

--- bookSearch/src/app/home/home.page.ts
+++ bookSearch/src/app/home/home.page.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+
+  constructor(private http: HttpClient){}
+  
+  HTTPREQUEST: string ="http://localhost:3000/books/";
+  searchText: string = "";
+  pageNumber: number = 0;
+
+  updateText(ev: any){
+    this.searchText = ev.target.value;
+    console.log(this.searchText);
+  }
+
+  getItems(){
+    this.getBooks();
+  }
+
+  getBooks(){
+    //hard
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+    this.http.get(this.HTTPREQUEST+this.searchText+"/"+this.pageNumber, {headers: headers})
+    .subscribe( data =>{
+      console.log(data)
+    })
+  }
+
+  nextPage(){
+    this.pageNumber ++;
+  }
+
+
 
 }
